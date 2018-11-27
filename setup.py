405eff73a5a6b16d69bf4075ce7f6bfb0899edde
Martin Atkins <mart@degeneration.co.uk>
2018-11-27 07:46:59 -0800

    Project boilerplate

---
+++ setup.py
@@ -0,0 +1,32 @@
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name="tfplugin",
+    version="dev",
+    author="Martin Atkins",
+    author_email="mart@degeneration.co.uk",
+    description="Implement Terraform plugins in Python",
+
+    packages=['tfplugin'],
+    install_requires=[
+    ],
+    setup_requires=[
+        'nose>=1.0',
+    ],
+    tests_require=[
+        'nose>=1.0',
+        'coverage',
+        'mock',
+        'pep8',
+    ],
+    test_suite='nose.collector',
+
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+    ],
+)
